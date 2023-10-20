9e1030c3f1bbce6c6eb3d991fbc3d7f44ca04cca
Gollavilli Dhanush Kumar <112778628+Dhanush2468@users.noreply.github.com>
2023-10-21 03:13:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,12 +17,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
-    
-            <div style="width: 200px; height: 30px;">
-  <a href="https://shrinkme.io/ref/110036665103617792226">
-    <img src="https://shrinkme.io/banners/ref/320x50.png" alt="Make short links and earn the biggest money" style="max-width: 100%; max-height: 100%;" />
-  </a>
-</div>
+  
       
     </div>
   );
